9746cfe8011bc14df03e43aa00255a51599cd2e1
wangpengwei <929494518@qq.com>
2018-05-16 18:29:26 +0800

    init project

---
+++ src/main/java/com/cloud/cloudall/CloudAllApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.cloudall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAllApplication.class, args);
+	}
+}
