6ba5c0ebe15f7b7256adf028c3e20272eec37341
wangpengwei <929494518@qq.com>
2018-05-25 16:02:47 +0800

    add zuul and turbine

--- cloud-turbine/src/main/java/com/cloud/cloudTurbine/CloudTurbineApplication.java
+++ cloud-turbine/src/main/java/com/cloud/cloudTurbine/CloudTurbineApplication.java
@@ -1,17 +1,16 @@
-package com.cloud.sleuthTrace2;
-
+package com.cloud.cloudTurbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-
-
+@EnableTurbine
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SleuthTrace2Application {
+public class CloudTurbineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SleuthTrace2Application.class, args);
+		SpringApplication.run(CloudTurbineApplication.class, args);
 	}
 }
---
+++ cloud-zuul/src/main/java/com/cloud/cloudzuul/CloudZuulApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.cloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class CloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudZuulApplication.class, args);
+	}
+}
