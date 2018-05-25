be964abcdcf7da1f2002ecfd51a264721f1c95e8
wangpengwei <929494518@qq.com>
2018-05-25 16:10:25 +0800

    use springCloudApplication

--- hello-service/src/main/java/com/cloud/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/cloud/helloservice/HelloServiceApplication.java
@@ -1,11 +1,10 @@
 package com.cloud.helloservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@EnableEurekaClient
-@SpringBootApplication
+
+@SpringCloudApplication
 public class HelloServiceApplication {
 
 	public static void main(String[] args) {
--- sleuth-trace-2/src/main/java/com/cloud/sleuthTrace2/SleuthTrace2Application.java
+++ sleuth-trace-2/src/main/java/com/cloud/sleuthTrace2/SleuthTrace2Application.java
@@ -2,13 +2,12 @@ package com.cloud.sleuthTrace2;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 
 
-@EnableDiscoveryClient
-@SpringBootApplication
+
+@SpringCloudApplication
 public class SleuthTrace2Application {
 
 	public static void main(String[] args) {
