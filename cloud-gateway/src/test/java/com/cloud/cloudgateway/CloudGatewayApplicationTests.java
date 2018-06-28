7de576e2a0149af39e1e255b78fc0d850cf49e7d
wangpengwei <929494518@qq.com>
2018-06-28 11:04:52 +0800

    add gateway

--- cloud-gateway/src/test/java/com/cloud/cloudgateway/CloudGatewayApplicationTests.java
+++ cloud-gateway/src/test/java/com/cloud/cloudgateway/CloudGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cloud.cloudzuul;
+package com.cloud.cloudgateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CloudZuulApplicationTests {
+public class CloudGatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
